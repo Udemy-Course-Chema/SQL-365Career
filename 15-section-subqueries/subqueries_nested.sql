c1cb18b25bf73a171a480db4bea945415e9afc7f
Chmox <chchema26@gmail.com>
2023-03-06 20:52:54 -0600

    Guardando cambios

--- 15-section-subqueries/subqueries_nested.sql
+++ 15-section-subqueries/subqueries_nested.sql
@@ -42,7 +42,7 @@ group by
 order by 
 	e.emp_no
 ;
-
+# =================================================================================
 # =================================================================================
 select A.* from 
 (select 
@@ -97,4 +97,92 @@ group by
 order by 
 	e.emp_no
 ) as B
-	;
+;
+
+# =================================================================================
+# EXERCISE
+# =================================================================================
+
+drop table if exists emp_manager;
+
+create table emp_manager(
+	emp_no int not null,
+    dept_no char(4) null, 
+    manager_no int not null
+);
+/**
+Fill emp_manager with data about employees, 
+the number of the department they are working in, 
+and their managers.
+*/
+select * from employees;
+select * from dept_emp;
+select * from dept_manager;
+
+insert into emp_manager 
+select 
+	u.*
+from 
+	(
+		select 
+			e.emp_no as employee_ID,
+			min(de.dept_no) as department_code,
+			(
+				select 
+					emp_no
+				from
+					dept_manager
+				where
+					emp_no = 110022
+			) as manager_ID
+		from
+			employees e
+		join
+			dept_emp de 
+				on 
+					e.emp_no = de.emp_no
+		where 
+			e.emp_no <= 10020
+		group by e.emp_no
+		order by e.emp_no
+	) as a
+union 
+-- Line 24
+	select 
+		b.*
+    from 
+    (
+		select 
+			e.emp_no as employee_ID,
+            min(de.dept_no) as department_code,
+            (
+				select 
+					emp_no
+				from
+					dept_manager
+				where 
+					emp_no = 110039
+            ) as manager_ID
+		from 
+			employees e
+		join 
+			dept_emp de 
+            on 
+				e.emp_no = de.emp_no
+		where 
+			e.emp_no = 110022
+		group by e.emp_no
+    ) as c
+	
+
+
+
+
+
+
+
+
+
+
+
+
